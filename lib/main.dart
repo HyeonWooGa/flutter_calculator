57f29919cfb45786883456a6558083178229487e
HyeonWooGa <yeonwoopark22@gmail.com>
2023-10-29 00:34:33 +0900

    ✨ Add Features Clear, Equal, +-*/ for just 2 Nums

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,62 @@ class _AppState extends State<App> {
   bool hasOperator = false;
   bool hasPostNum = false;
 
-  void onClickHistory() {}
+  void onClickVoid() {}
+
+  void onClickClear() {
+    preNum = 0;
+    postNum = 0;
+    result = 0;
+    hasPreNum = false;
+    hasOperator = false;
+    hasPostNum = false;
+    setState(() {});
+  }
+
+  void onClickNum(numClicked) {
+    if (hasPreNum) {
+      postNum = numClicked;
+      switch (operator) {
+        case '+':
+          result = preNum + postNum;
+          break;
+        case '-':
+          result = preNum - postNum;
+          break;
+        case 'X':
+          result = preNum * postNum;
+          break;
+        case '/':
+          result = preNum / postNum;
+          break;
+        default:
+          break;
+      }
+      hasPostNum = true;
+    } else {
+      preNum = numClicked;
+      hasPreNum = true;
+    }
+    setState(() {});
+  }
+
+  void onClickOperator(operatorClicked) {
+    if (!hasOperator) {
+      operator = operatorClicked;
+      hasOperator = true;
+    }
+    setState(() {});
+  }
+
+  void onClickEqual() {
+    hasPostNum = false;
+    hasOperator = false;
+    postNum = 0;
+    operator = '';
+    preNum = result;
+    result = 0;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +90,20 @@ class _AppState extends State<App> {
                 children: [
                   Text(
                     '$preNum',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 56,
+                    ),
+                  ),
+                  Text(
+                    hasOperator ? operator : '',
+                    style: const TextStyle(
+                      color: Colors.green,
+                      fontSize: 56,
+                    ),
+                  ),
+                  Text(
+                    hasPostNum ? '$postNum' : '',
                     style: const TextStyle(
                       color: Colors.white,
                       fontSize: 56,
@@ -59,7 +128,7 @@ class _AppState extends State<App> {
                   Row(
                     children: [
                       IconButton(
-                        onPressed: onClickHistory,
+                        onPressed: onClickVoid,
                         icon: const Icon(
                           Icons.history_rounded,
                           color: Colors.white54,
@@ -67,7 +136,7 @@ class _AppState extends State<App> {
                       ),
                       const SizedBox(width: 20),
                       IconButton(
-                        onPressed: onClickHistory,
+                        onPressed: onClickVoid,
                         icon: const Icon(
                           Icons.straighten_rounded,
                           color: Colors.white54,
@@ -75,7 +144,7 @@ class _AppState extends State<App> {
                       ),
                       const SizedBox(width: 20),
                       IconButton(
-                        onPressed: onClickHistory,
+                        onPressed: onClickVoid,
                         icon: const Icon(
                           Icons.calculate_outlined,
                           color: Colors.white54,
@@ -86,7 +155,7 @@ class _AppState extends State<App> {
                   Row(
                     children: [
                       IconButton(
-                        onPressed: onClickHistory,
+                        onPressed: onClickVoid,
                         icon: const Icon(
                           Icons.backspace_outlined,
                           color: Colors.green,
@@ -108,7 +177,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: onClickClear,
                       child: const ButtonCalc(
                         text: 'C',
                         color: Colors.red,
@@ -122,7 +191,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: onClickVoid,
                       child: const ButtonCalc(
                         text: '()',
                         color: Colors.green,
@@ -136,7 +205,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: onClickVoid,
                       child: const ButtonCalc(
                         text: '%',
                         color: Colors.green,
@@ -150,7 +219,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickOperator('/'),
                       child: const ButtonCalc(
                         text: '/',
                         color: Colors.green,
@@ -170,7 +239,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickNum(7),
                       child: const ButtonCalc(
                         text: '7',
                       ),
@@ -183,7 +252,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickNum(8),
                       child: const ButtonCalc(
                         text: '8',
                       ),
@@ -196,7 +265,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickNum(9),
                       child: const ButtonCalc(
                         text: '9',
                       ),
@@ -209,7 +278,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickOperator('X'),
                       child: const ButtonCalc(
                         text: 'X',
                         color: Colors.green,
@@ -229,7 +298,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickNum(4),
                       child: const ButtonCalc(
                         text: '4',
                       ),
@@ -242,7 +311,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickNum(5),
                       child: const ButtonCalc(
                         text: '5',
                       ),
@@ -255,7 +324,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickNum(6),
                       child: const ButtonCalc(
                         text: '6',
                       ),
@@ -268,7 +337,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickOperator('-'),
                       child: const ButtonCalc(
                         text: 'ㅡ',
                         color: Colors.green,
@@ -288,7 +357,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickNum(1),
                       child: const ButtonCalc(
                         text: '1',
                       ),
@@ -301,7 +370,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickNum(2),
                       child: const ButtonCalc(
                         text: '2',
                       ),
@@ -314,7 +383,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickNum(3),
                       child: const ButtonCalc(
                         text: '3',
                       ),
@@ -327,7 +396,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickOperator('+'),
                       child: const ButtonCalc(
                         text: '+',
                         color: Colors.lightGreen,
@@ -347,7 +416,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: onClickVoid,
                       child: const ButtonCalc(
                         text: '+/-',
                       ),
@@ -360,7 +429,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: () => onClickNum(0),
                       child: const ButtonCalc(
                         text: '0',
                       ),
@@ -373,7 +442,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: onClickVoid,
                       child: const ButtonCalc(
                         text: '.',
                       ),
@@ -386,7 +455,7 @@ class _AppState extends State<App> {
                     ),
                     padding: const EdgeInsets.all(9),
                     child: TextButton(
-                      onPressed: onClickHistory,
+                      onPressed: onClickEqual,
                       child: const ButtonCalc(
                         text: '=',
                       ),
